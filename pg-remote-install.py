1608dc72058735cccdd7695f176f06db201495cc
Uliana Khmeleva <ulechka.hmeleva@gmail.com>
2024-05-14 14:58:16 +0700

    use os to run local comands

--- pg-remote-install.py
+++ pg-remote-install.py
@@ -1,6 +1,7 @@
 import argparse
 import subprocess
 from installer import Installer
+import os
 
 # Parse parameters
 # programm has 4 parameters:
@@ -31,18 +32,18 @@ installer = Installer(host, ssh_login, db_login, db_user)
 
 # run local script to check DB is working
 
-# add_pass='echo \"{}:5432:mydb:{}:paroleparole\" >> ~/.pgpass'.format(host, db_user)
-# print(add_pass)
-# subprocess.run(add_pass)
-# chmod="chmod 0600 ~/.pgpass"
-# print(chmod)
-# subprocess.run(chmod)
-#
-# select="psql -h {} -p 5432 -U {} -w -d mydb -c \'SELECT 1;\'".format(host, db_user)
-# print(select)
-# subprocess.run(select)
-#
-# print("you can run the following command on your terminal to chech if db is available:\n {}".format(select))
+add_pass='echo \"{}:5432:mydb:{}:paroleparole\" >> ~/.pgpass'.format(host, db_user)
+print(add_pass)
+os.system(add_pass)
+chmod="chmod 0600 ~/.pgpass"
+print(chmod)
+os.system(chmod)
+
+select="psql -h {} -p 5432 -U {} -w -d mydb -c \"SELECT 1;\"".format(host, db_user)
+print(select)
+os.system(select)
+
+print("You can run the following command on your terminal to chech if db is available:\n {}".format(select))
 
 
 
